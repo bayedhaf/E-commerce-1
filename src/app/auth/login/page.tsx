9b7f57e770e91afd720a46d9b3dbcea7fadbcf6a
bayedhaf <bayedhaf2023@gmail.com>
2025-07-07 08:01:52 -0400

    login page added

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,9 +1,28 @@
+"use client"
+
 import React from 'react'
+
 
 const login = () => {
   return (
-    <div>
-      login page
+    <div className='bg-black text-white '>
+      <div className="">
+        <div className="">
+          <form action="" className="">
+            <h1 className="text-">Login</h1>
+            <div className="">
+              <label htmlFor="">Email</label><br/>
+              <input type="text" className="" id="email" name="email" required placeholder="Enter your email..."/>
+              <br/>
+              <label htmlFor="">Password:</label><br/>
+              <input type="text" className="" id="password" name="password" required placeholder="Enter your password ..."/>
+              <br/>
+              <input type="submit" className="" placeholder="Submit"/>
+            </div>
+          </form>
+        </div>
+      </div>
+      
     </div>
   )
 }
